cd732d04373ad28fb5676ec71cb599736277902c
hunglv97 <you@example.com>
2021-08-19 21:24:44 +0700

    upload giayxin3s

---
+++ js/owl-setting.js
@@ -0,0 +1,57 @@
+$(function () {
+    $(".slider_main").owlCarousel({
+        items: 1,
+        responsive: {
+            1200: { item: 1, },// breakpoint from 1200 up
+            982: { items: 1, },
+            768: { items: 1, },
+            480: { items: 1, },
+            0: { items: 1, }
+        },
+        rewind: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        smartSpeed: 500, //slide speed smooth
+
+        dots: true,
+        dotsEach: false,
+        loop: true,
+        nav: true,
+        navText: ['', ''],
+        // navSpeed: 250, //slide speed when click button
+
+        autoWidth: false,
+        margin: 10,
+
+        lazyContent: false,
+        lazyLoad: false,
+
+        animateIn: false,
+        animateOut: false,
+
+        center: false,
+        URLhashListener: false,
+
+        video: false,
+        videoHeight: false,
+        videoWidth: false,
+    });
+    $('.slider_prod').owlCarousel({
+        items: 5,
+        responsive: {
+            1200: { item: 5, },// breakpoint from 1200 up
+            982: { items: 4, },
+            768: { items: 3, },
+            480: { items: 2, },
+            0: { items: 1, }
+        },
+        loop: true,
+        nav: true,
+        navText: ['', ''],
+        dots: true,
+        dotsEach: true,
+
+    });
+
+});
